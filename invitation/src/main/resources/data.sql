7c59d764f963367a55737d7808a9724674d0334e
JungHyeonKang <rkdwjdgus0103@gmail.com>
2023-10-14 00:46:24 +0900

    프로젝트 셋팅
    스프링부트, JPA, H2 인메모리, 테스트 환경 셋팅 완료

---
+++ invitation/src/main/resources/data.sql
@@ -0,0 +1,11 @@
+
+DROP TABLE IF EXISTS testTable;
+
+CREATE TABLE testTable
+(
+  id INT ,
+  MEMBER_NAME varchar(255),
+  MEMBER_PHONE varchar(255),
+  MEMBER_EMAIL varchar (255),
+  MEMBER_STATUS varchar (100)
+);
